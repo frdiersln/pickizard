b9dc3f55e70199e883a16be5df4b232c5a14cdf6
Ferdi Eraslan <frdiersln@hotmail.com>
2025-02-05 22:19:31 +0300

    content area margins set

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello Pickizard</h1>
+      <div className="container mx-auto w-[90%] md:w-1/2">
+        <h1>Hello Pickizard!</h1>
+      </div>
     </main>
   )
 }
